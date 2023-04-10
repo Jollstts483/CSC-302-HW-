7aa45c117ce33718d56e3fb5bc78643b24439051
Alizia Hamilton <127693200+Jollstts483@users.noreply.github.com>
2023-04-09 21:50:21 -0400

    Add files via upload

---
+++ TimeSeriesTrend.R
@@ -0,0 +1,34 @@
+library(magrittr)
+library(tidyverse)
+library(ggplot2)
+
+
+load("C:/Users/Hamil/OneDrive/Desktop/preprint_growth.rda")
+head(preprint_growth)
+
+preprint_growth %>% filter(archive == "bioRxiv") %>%
+  filter(count > 0) -> biorxiv_growth
+preprints<-preprint_growth %>% filter(archive %in%
+                                        c("bioRxiv", "arXiv q-bio", "PeerJ Preprints")) %>%filter(count > 0) %>%
+  mutate(archive = factor(archive, levels = c("bioRxiv", "arXiv q-bio", "PeerJ Preprints")))
+1
+preprints_final <- filter(preprints, date == ymd("2017-01-01"))
+ggplot(preprints) +
+  aes(date, count, color = archive, fill = archive) +
+  geom_line(size = 1) +
+  scale_y_continuous(
+    limits = c(0, 600), expand = c(0, 0),
+    name = "preprints / month",
+    sec.axis = dup_axis( #this part is for the second y axis
+      breaks = preprints_final$count, #and we use the counts to position our labels
+      labels = c("arXivq-bio", "PeerJPreprints", "bioRxiv"),
+      name = NULL)
+  ) +
+  scale_x_date(name = "year",
+               limits = c(ymd("2012-01-01"), ymd("2018-01-01"))) + # Changed start date
+  scale_color_manual(values = c("#3B4CC0", "#6699CC", "#99CCFF"), # Changed color values
+                     name = NULL) +
+  theme(legend.position = "none") 
+
+
+
